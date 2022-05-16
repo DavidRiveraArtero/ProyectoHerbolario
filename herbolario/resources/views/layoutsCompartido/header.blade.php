9560037e6c946ebc7104f8d3c65812f3270da407
resapa1234 <pasare@fp.insjoaquimmir.cat>
2022-05-16 16:39:52 +0200

    Iconos menu

--- herbolario/resources/views/layoutsCompartido/header.blade.php
+++ herbolario/resources/views/layoutsCompartido/header.blade.php
@@ -39,21 +39,21 @@
                     <div class="item">
                         <a class="" href="">Menu User<i class="fas fa-angle-right dropdown"></i></a>
                         <div class="sub-menu">
-                            <a class="dropdown-item sub-item" href="{{route("perfil.show",Auth::user())}}"><h5>Mi cuenta</h5></a>
+                            <a class="dropdown-item sub-item" href="{{route("perfil.show",Auth::user())}}"><h5>Mi cuenta<i style="float: right;" class="fa-solid fa-house-user"></i></h5></a>
 
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
-                                <h5>{{ __('Logout') }}</h5>
+                                <h5>{{ __('Logout') }} <i style="float: right" class="fa-solid fa-arrow-right-from-bracket"></i></h5>
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
 
-                            <a class="dropdown-item sub-item" href="{{route("carrito.index",Auth::user())}}"><h5>Carrito</h5></a>
+                            <a class="dropdown-item sub-item" href="{{route("carrito.index",Auth::user())}}"><h5>Carrito<i style="float: right" class="fa-solid fa-cart-arrow-down"></i></h5></a>
                             @if(Auth::user()->role_id == 1)
-                                <a class="dropdown-item sub-item" href="{{route("productos.index")}}"><h5>Intranet</h5></a>
+                                <a class="dropdown-item sub-item" href="{{route("productos.index")}}"><h5>Intranet <i style="float: right" class="fa-solid fa-user-secret"></i></h5></a>
                             @endif
                         </div>
                     </div>
